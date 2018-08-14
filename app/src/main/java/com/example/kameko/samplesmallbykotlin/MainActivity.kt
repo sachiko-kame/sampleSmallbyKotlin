b5854816a9c0c38dcabe918e94f94f02db4cd372
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 14:15:40 +0900

    clean

--- app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
+++ app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
@@ -8,8 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val numArray = arrayListOf(1, 2, 3, 4, 5)
-        println(numArray.size)
     }
 }
