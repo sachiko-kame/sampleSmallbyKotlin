6d6a71229e05c774de686eeea538c3eda82a2b97
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 13:39:24 +0900

    2,extension

--- app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
+++ app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
@@ -5,8 +5,15 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val sampleText:String = "もしもし"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sampleText.text()
     }
+}
+
+fun String.text() {
+    println("自分のテキストは$this")
 }
