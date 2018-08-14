76b972297351e84d84e49b8585eddb2cce3cc98f
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 14:29:26 +0900

    5,クラスの作成

--- app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
+++ app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
@@ -5,8 +5,23 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val nameClass = NameClass("カモメ")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nameClass.NameText()
+    }
+}
+
+class NameClass(name: String) {
+    var name: String = ""
+
+    init {
+        this.name = name
+    }
+
+    fun NameText(){
+        println(this.name)
     }
 }
