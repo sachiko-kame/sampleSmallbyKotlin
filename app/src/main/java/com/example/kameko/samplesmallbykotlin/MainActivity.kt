622edd9b7f00e26d1162d58fbb0aa8d96289f8b7
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 13:31:54 +0900

    1,enum

--- app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
+++ app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.kameko.samplesmallbykotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val animals = arrayListOf(aString.dog, aString.cat, aString.turtle)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (one in animals){
+            when (one) {
+                aString.dog -> println("犬か")
+                aString.cat -> println("猫か")
+                else -> {
+                    println("亀かもしれない")
+                }
+            }
+        }
     }
+}
+
+enum class aString(val text: String){
+    dog("犬"),
+    cat("猫"),
+    turtle("かめ")
 }
