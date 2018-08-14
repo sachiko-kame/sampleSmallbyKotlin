ef9b44be857531731ef747bd635e85fc2983ba1e
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 15:16:21 +0900

    7,Interface

--- app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
+++ app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
@@ -5,8 +5,29 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val child = Child()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        child.bar()
+        child.foo()
+
+    }
+}
+
+interface MyInterface {
+    fun bar()
+    fun foo()
+}
+
+
+class Child : MyInterface {
+    override fun bar() {
+       println("バー")
+    }
+    override fun foo(){
+        println("ふー")
     }
 }
