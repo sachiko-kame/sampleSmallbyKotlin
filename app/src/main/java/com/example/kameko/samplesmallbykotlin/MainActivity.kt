4d2985a39d286219a15ef5e7026f18edf0e17c33
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 15:08:02 +0900

    6,getとset

--- app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
+++ app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
@@ -5,8 +5,18 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    var sampleString: String
+        get() = this.toString()
+        set(value) {
+            println("あてはめました！$value")
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(sampleString)
+        sampleString = "カメ吉です"
+        println(sampleString)
     }
 }
