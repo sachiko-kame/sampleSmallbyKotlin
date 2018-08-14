41dfec39bce79e1d9ed58890133a84bb16d99f61
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2018-08-14 14:14:00 +0900

    4.配列数カウント

--- app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
+++ app/src/main/java/com/example/kameko/samplesmallbykotlin/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numArray = arrayListOf(1, 2, 3, 4, 5)
+        println(numArray.size)
     }
 }
